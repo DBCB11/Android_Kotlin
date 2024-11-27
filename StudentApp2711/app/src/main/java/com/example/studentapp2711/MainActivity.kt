0a20f75af0c2273183d9784eba9d02ec5df7a0b2
Nong Duc Huy <nongduchuy10@gmail.com>
2024-11-27 23:17:06 +0700

    2711

---
+++ StudentApp2711/app/src/main/java/com/example/studentapp2711/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.example.studentapp2711
+
+import android.os.Bundle
+import android.util.Log
+import android.view.ContextMenu
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+            }
+
+        val students = mutableListOf(
+            StudentModel("Nguyễn Văn An", "SV001"),
+            StudentModel("Trần Thị Bảo", "SV002"),
+            StudentModel("Lê Hoàng Cường", "SV003"),
+            StudentModel("Phạm Thị Dung", "SV004"),
+            StudentModel("Đỗ Minh Đức", "SV005"),
+            StudentModel("Vũ Thị Hoa", "SV006"),
+            StudentModel("Hoàng Văn Hải", "SV007"),
+            StudentModel("Bùi Thị Hạnh", "SV008"),
+            StudentModel("Đinh Văn Hùng", "SV009"),
+            StudentModel("Nguyễn Thị Linh", "SV010"),
+            StudentModel("Phạm Văn Long", "SV011"),
+            StudentModel("Trần Thị Mai", "SV012"),
+            StudentModel("Lê Thị Ngọc", "SV013"),
+            StudentModel("Vũ Văn Nam", "SV014"),
+            StudentModel("Hoàng Thị Phương", "SV015"),
+            StudentModel("Đỗ Văn Quân", "SV016"),
+            StudentModel("Nguyễn Thị Thu", "SV017"),
+            StudentModel("Trần Văn Tài", "SV018"),
+            StudentModel("Phạm Thị Tuyết", "SV019"),
+            StudentModel("Lê Văn Vũ", "SV020")
+        )
+
+        val studentAdapter = StudentAdapter(students)
+
+        findViewById<RecyclerView>(R.id.recycler_view_students).run {
+            adapter = studentAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+        }
+
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        super.onCreateContextMenu(menu, v, menuInfo)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val pos = (item.menuInfo as AdapterView.AdapterContextMenuInfo).position
+        when (item.itemId) {
+            R.id.action_add -> {Toast.makeText(this, "add $pos", Toast.LENGTH_LONG).show()}
+            R.id.action_remove -> {Toast.makeText(this, "remove $pos", Toast.LENGTH_LONG).show()}
+            R.id.action_edit -> {Toast.makeText(this, "edit $pos", Toast.LENGTH_LONG).show()}
+        }
+        return super.onContextItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_add -> {Toast.makeText(this, "Share", Toast.LENGTH_LONG).show()}
+            R.id.action_remove -> {Toast.makeText(this, "Download", Toast.LENGTH_LONG).show()}
+            R.id.action_edit -> {Toast.makeText(this, "Settings", Toast.LENGTH_LONG).show()}
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
---
+++ StudentApp2711/app/src/main/java/com/example/studentapp2711/StudentModel.kt
@@ -0,0 +1,2 @@
+package com.example.studentapp2711
+data class StudentModel(val studentName: String, val studentId: String)
