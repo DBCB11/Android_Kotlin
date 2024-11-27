1efcdd8d09b841c4e33796825bc0d9c58bece1c7
Nong Duc Huy <nongduchuy10@gmail.com>
2024-11-27 23:50:36 +0700

    update add

--- StudentApp2711/app/src/main/java/com/example/studentapp2711/MainActivity.kt
+++ StudentApp2711/app/src/main/java/com/example/studentapp2711/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.studentapp2711
 
+import android.app.Instrumentation
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.ContextMenu
@@ -13,6 +15,9 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,6 +27,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var launcherAdd: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -62,25 +68,22 @@ class MainActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(this@MainActivity)
         }
 
-    }
+        launcherAdd = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+            { it: ActivityResult ->
+                if (it.resultCode == RESULT_OK) {
+                    val hoten = it.data?.getStringExtra("hoten")
+                    val mssv = it.data?.getStringExtra("mssv")
+                    if (!hoten.isNullOrEmpty() && !mssv.isNullOrEmpty()) {
+                        val newStudent = StudentModel(hoten, mssv)
+                        students.add(newStudent)
+                        studentAdapter.notifyDataSetChanged()
+                    } else {
+                        Toast.makeText(this, "Không nhận được dữ liệu sinh viên", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
 
-    override fun onCreateContextMenu(
-        menu: ContextMenu?,
-        v: View?,
-        menuInfo: ContextMenu.ContextMenuInfo?
-    ) {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        super.onCreateContextMenu(menu, v, menuInfo)
-    }
-
-    override fun onContextItemSelected(item: MenuItem): Boolean {
-        val pos = (item.menuInfo as AdapterView.AdapterContextMenuInfo).position
-        when (item.itemId) {
-            R.id.action_add -> {Toast.makeText(this, "add $pos", Toast.LENGTH_LONG).show()}
-            R.id.action_remove -> {Toast.makeText(this, "remove $pos", Toast.LENGTH_LONG).show()}
-            R.id.action_edit -> {Toast.makeText(this, "edit $pos", Toast.LENGTH_LONG).show()}
-        }
-        return super.onContextItemSelected(item)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -90,7 +93,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.action_add -> {Toast.makeText(this, "Share", Toast.LENGTH_LONG).show()}
+            R.id.action_add -> {
+                val intent = Intent(this, AddStudentActivity::class.java)
+                launcherAdd.launch(intent)
+            }
             R.id.action_remove -> {Toast.makeText(this, "Download", Toast.LENGTH_LONG).show()}
             R.id.action_edit -> {Toast.makeText(this, "Settings", Toast.LENGTH_LONG).show()}
         }
